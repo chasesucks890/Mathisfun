97653511f7b063b0c754826883c146460c014301
chasesucks890 <146029821+chasesucks890@users.noreply.github.com>
2024-03-11 23:39:44 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,95 +1,51 @@
-import express from 'express'
-import basicAuth from 'express-basic-auth'
-import http from 'node:http'
-import { createBareServer } from '@tomphttp/bare-server-node'
-import path from 'node:path'
-import cors from 'cors'
-import config from './config.js'
-const __dirname = process.cwd()
-const server = http.createServer()
-const app = express(server)
-const bareServer = createBareServer('/v/')
-const PORT = process.env.PORT || 8080
-if (config.challenge) {
-  console.log('Password protection is enabled. Usernames are: ' + Object.keys(config.users))
-  console.log('Passwords are: ' + Object.values(config.users))
-
-  app.use(
-    basicAuth({
-      users: config.users,
-      challenge: true,
-    })
-  )
-}
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors())
-app.use(express.static(path.join(__dirname, 'static')))
-
-if (config.routes !== false) {
-  const routes = [
-    { path: '/~', file: 'apps.html' },
-    { path: '/-', file: 'games.html' },
-    { path: '/!', file: 'settings.html' },
-    { path: '/0', file: 'tabs.html' },
-    { path: '/1', file: 'go.html' },
-    { path: '/', file: 'index.html' },
-  ]
-
-  routes.forEach((route) => {
-    app.get(route.path, (req, res) => {
-      res.sendFile(path.join(__dirname, 'static', route.file))
-    })
+window.addEventListener('load', () => {
+  navigator.serviceWorker.register('../sw.js?v=1', {
+    scope: '/a/',
   })
-}
-if (config.local !== false) {
-  app.get('/y/*', (req, res, next) => {
-    const baseUrl = 'https://raw.githubusercontent.com/ypxa/y/main'
-    fetchData(req, res, next, baseUrl)
-  })
+})
 
-  app.get('/f/*', (req, res, next) => {
-    const baseUrl = 'https://raw.githubusercontent.com/4x-a/x/fixy'
-    fetchData(req, res, next, baseUrl)
+const form = document.getElementById('fs')
+const input = document.getElementById('is')
+
+if (form && input) {
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault()
+    processUrl(input.value, '/1')
   })
 }
 
-const fetchData = async (req, res, next, baseUrl) => {
-  try {
-    const reqTarget = `${baseUrl}/${req.params[0]}`
-    const asset = await fetch(reqTarget)
+function processUrl(value, path) {
+  let url = value.trim()
+  const engine = localStorage.getItem('engine')
+  const searchUrl = engine ? engine : 'https://www.google.com/search?q='
 
-    if (asset.ok) {
-      const data = await asset.arrayBuffer()
-      res.end(Buffer.from(data))
-    } else {
-      next()
-    }
-  } catch (error) {
-    console.error('Error fetching:', error)
-    next(error)
+  if (!isUrl(url)) {
+    url = searchUrl + url
+  } else if (!(url.startsWith('https://') || url.startsWith('http://'))) {
+    url = 'https://' + url
   }
-}
-server.on('request', (req, res) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeRequest(req, res)
+
+  sessionStorage.setItem('GoUrl', __uv$config.encodeUrl(url))
+  const dy = localStorage.getItem('dy')
+
+  if (path) {
+    location.href = path
+  } else if (dy === 'true') {
+    window.location.href = '/a/q/' + __uv$config.encodeUrl(url)
   } else {
-    app(req, res)
+    window.location.href = '/a/' + __uv$config.encodeUrl(url)
   }
-})
+}
 
-server.on('upgrade', (req, socket, head) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeUpgrade(req, socket, head)
-  } else {
-    socket.end()
-  }
-})
+function go(value) {
+  processUrl(value, '/1')
+}
 
-server.on('listening', () => {
-  console.log(`Running at http://localhost:${PORT}`)
-})
+function blank(value) {
+  processUrl(value)
+}
 
-server.listen({
-  port: PORT,
-})
+function isUrl(val = '') {
+  if (/^http(s?):\/\//.test(val) || (val.includes('.') && val.substr(0, 1) !== ' ')) return true
+  return false
+}
